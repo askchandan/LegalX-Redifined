299cd06413ba891c9ce32346af74c5f79cff6c5b
askchandan <chandanmalakar6209@gmail.com>
2025-10-27 21:23:47 +0530

    redifining the deployment

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Install setuptools first to avoid build_meta import errors
-pip install setuptools>=65.0.0 wheel
-# Then install the rest of the requirements
-pip install -r requirements.txt
